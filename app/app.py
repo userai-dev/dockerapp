d286bb694b5fe75f6480c8226d5d833ff40aeacc
userai-dev <79525179+userai-dev@users.noreply.github.com>
2023-01-20 20:22:04 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ default_key = '1'
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 cache.set(default_key, "one")
 
+
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
@@ -21,6 +22,7 @@ def mainpage():
 		cache_value = cache.get(key).decode('utf-8')
 
 	return render_template('index.html', key=key, cache_value=cache_value)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
